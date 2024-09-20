6cba2a2edb9d1121cd02f274bac7f78dd2e49389
Raul Exposito Campos <raulrecrec@gmail.com>
2024-09-20 12:14:48 +0200

    ft_memchr norm correction

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,19 +6,20 @@
 /*   By: rexposit <rexposit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:51:47 by rexposit          #+#    #+#             */
-/*   Updated: 2024/09/20 12:11:23 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/09/20 12:13:35 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>  //SUSTITUIR LIBRERIA CON LIBFT.H
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*s_cpy;
 	unsigned char	c_cpy;
+
 	i = 0;
 	c_cpy = (unsigned char) c;
-	s_cpy = (unsigned char*) s;
+	s_cpy = (unsigned char *) s;
 	while (i < n)
 	{
 		if (s_cpy[i] == c_cpy)
