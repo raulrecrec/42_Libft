13ded3a2534c8101dad835f0fc1b324bcc80daaa
Raul Exposito Campos <raulrecrec@gmail.com>
2024-09-20 12:20:31 +0200

    variable order upgrade

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,16 +6,16 @@
 /*   By: rexposit <rexposit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 19:51:47 by rexposit          #+#    #+#             */
-/*   Updated: 2024/09/20 12:13:35 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/09/20 12:20:12 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>  //SUSTITUIR LIBRERIA CON LIBFT.H
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
 	unsigned char	*s_cpy;
 	unsigned char	c_cpy;
+	size_t			i;
 
 	i = 0;
 	c_cpy = (unsigned char) c;
