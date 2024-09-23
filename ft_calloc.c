2a8a37ccdc77cc4450bf30bec41dc3b0c8f811bb
Raul Exposito Campos <raulrecrec@gmail.com>
2024-09-23 13:23:17 +0200

    function calloc replicated

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/20 13:42:14 by rexposit          #+#    #+#             */
+/*   Updated: 2024/09/23 13:23:03 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>  //SUSTITUIR LIBRERIA CON LIBFT.H PARA FT_MEMSET Y SIZE_T
+#include <stdlib.h> //REVISAR SI BORRAR O DEJARLO
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t	mem_size;
+	void	*ptr;
+
+	if (nmemb != 0 && size > __SIZE_MAX__ / nmemb)
+		return (NULL);
+	mem_size = nmemb * size;
+	ptr = malloc(mem_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_memset(ptr, 0, mem_size);
+	return (ptr);
+}
