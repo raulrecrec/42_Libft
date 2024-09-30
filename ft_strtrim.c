51e1a24dc512d1d833f983fff72bb784c268e1a9
Raul Exposito Campos <raulrecrec@gmail.com>
2024-09-30 16:27:02 +0200

    Implemented ft_strtrim with specific requirements

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,51 +6,93 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 13:39:25 by rexposit          #+#    #+#             */
-/*   Updated: 2024/09/27 20:46:09 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/09/30 16:24:27 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h> //SUSTITUIR LIBRERIA CON LIBFT.H
 #include <stdlib.h> //REVISAR SI BORRAR O DEJARLO
 int	ft_strlen(const char *str);
-
-int	strcmp_temp(char const *s1, char const *set)
+int	strcmp_forward(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
+	size_t			i;
+	size_t			j;
+	size_t			x;
+	unsigned int	found;
 
 	i = 0;
-	while (s1[i + j] != set[j])
+	x = 0;
+	while (s1[i] != '\0')
 	{
 		j = 0;
-		while (s1[i + j] != set[j] && set[j] != '\0')
+		found = 0;
+		while (set[j] != '\0' && found == 0)
+		{
+			if (s1[i] == set[j])
+			{
+				x++;
+				found = 1;
+			}
 			j++;
+		}
+		if (found != 1)
+			break;
 		i++;
 	}
-	return (s1[i + j] != set[j]);
+	return (x);
+}
+int	strcmp_reverse(char const *s1, char const *set)
+{
+	size_t			i;
+	size_t			j;
+	size_t			x;
+	unsigned int	found;
+
+	i = ft_strlen(s1) - 1;
+	x = 0;
+	while (i > 0)
+	{
+		j = 0;
+		found = 0;
+		while (set[j] != '\0' && found == 0)
+		{
+			if (s1[i] == set[j])
+			{
+				x++;
+				found = 1;
+			}
+			j++;
+		}
+		if (found != 1)
+			break;
+		i--;
+	}
+	return (x);
 }
 
 char *ft_strtrim(char const *s1, char const *set)
 {
+	size_t	str_left;
+	size_t	str_right;
 	size_t	i;
-	size_t	j;
-	size_t	str_start;
-	size_t	str_end;
+	size_t	len_s1_trim;
 	char	*s1_trim;
 
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	str_left = strcmp_forward(s1, set);
+	str_right = strcmp_reverse(s1, set);
+	len_s1_trim = ft_strlen(s1) - str_left - str_right;
+	s1_trim = malloc(len_s1_trim + 1);
+	if (s1_trim == NULL)
+		return (NULL);
 	i = 0;
-	j = 0;
-	str_start = 0;
-	str_end = 0;
-	while (s1[i] != '\0')
+	while (i < len_s1_trim)
 	{
-		j = 0;
-		while (set[j] != '\0')
-		{
-			if (s1[i + j] == set[j])
-				str_start++;
-			j++;
-		}
+		s1_trim[i] = s1[str_left];
 		i++;
+		str_left++;
 	}
+	s1_trim[i] = '\0';
+	return (s1_trim);
 }
