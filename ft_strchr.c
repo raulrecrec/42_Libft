02b136e55afc2f67ba2b53d53bec415493684dab
Raul Exposito Campos <raulrecrec@gmail.com>
2024-09-30 21:18:11 +0200

    function upgrade to take in consideration '\0' cases

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,11 +6,12 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 18:05:24 by rexposit          #+#    #+#             */
-/*   Updated: 2024/09/23 14:25:43 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/09/30 21:07:36 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>  //SUSTITUIR LIBRERIA CON LIBFT.H
+#include "libft.h"
+
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
@@ -18,9 +19,11 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char) c)
 			return ((char *)&s[i]);
 		i++;
 	}
-	return ((void *) 0);
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
 }
