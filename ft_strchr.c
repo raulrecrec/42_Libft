8cd13ba988c2c9d6ef3013e32a5a784457a3bb63
Raul Exposito Campos <raulrecrec@gmail.com>
2024-10-07 12:37:51 +0200

    function optimized making less casts

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 18:05:24 by rexposit          #+#    #+#             */
-/*   Updated: 2024/09/30 21:07:36 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/03 18:38:58 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,17 @@
 char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
+	char	c_cpy;
 
 	i = 0;
+	c_cpy = (char) c;
 	while (s[i] != '\0')
 	{
-		if (s[i] == (char) c)
+		if (s[i] == c_cpy)
 			return ((char *)&s[i]);
 		i++;
 	}
-	if (c == '\0')
+	if (c_cpy == '\0')
 		return ((char *)&s[i]);
 	return (NULL);
 }
