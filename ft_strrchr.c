ed871376b0017093654a4cbdb38d0a74c7c134d6
Raul Exposito Campos <raulrecrec@gmail.com>
2024-10-08 14:28:08 +0200

    probllem reading out of limits fixed

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 20:26:27 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/07 12:48:01 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/08 14:22:12 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char	*ft_strrchr(const char *s, int c)
 	int		i;
 	char	c_cpy;
 
-	i = ft_strlen(s) + 1;
+	i = ft_strlen(s);
 	c_cpy = (char) c;
+	if (c_cpy == '\0')
+		return ((char *)&s[i]);
 	while (i >= 0)
 	{
 		if (s[i] == c_cpy)
 			return ((char *)&s[i]);
 		i--;
 	}
-	if (c_cpy == '\0')
-		return ((char *)&s[i]);
 	return (NULL);
 }
