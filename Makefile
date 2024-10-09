5c0987a5104e145966d00504453171c15e3db197
Raul VM <raulrecrec@gmail.com>
2024-10-09 14:51:25 +0200

    ft_putendl_fd added

--- Makefile
+++ Makefile
@@ -34,7 +34,8 @@ SRCS = \
 	ft_strmapi.c	\
 	ft_striteri.c	\
 	ft_putchar_fd.c	\
-	ft_putstr_fd.c	
+	ft_putstr_fd.c	\
+	ft_putendl_fd.c	
 
 OBJS = $(SRCS:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:08:58 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/09 14:41:31 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/09 14:49:52 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void ft_putchar_fd(char c, int fd);
 void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
 
 #endif
